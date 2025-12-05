2aa80c8b808fe83393af5d928949f4f3bb6a2ecb
pratik <pratik@sivreninnovations.com>
2025-12-05 12:11:10 +0530

    dfgfdg

--- app/Http/Controllers/AuthorizeCreationController.php
+++ app/Http/Controllers/AuthorizeCreationController.php
@@ -27,7 +27,9 @@ class AuthorizeCreationController extends Controller
         $input_url_encoded_string = str_replace('%25', '%', $input_url_encoded_string);
         $input_url_encoded_string = str_replace('%20', ' ', $input_url_encoded_string);
         $signature = $egiroService->encodeURIComponent(
-            $this->sign($input_url_encoded_string, $request->client_slug)
+            $egiroService->encodeURIComponent(
+                $gpgService->sign($input_url_encoded_string, config('clients.' . $request->client_slug . '.' . env('APP_ENV') . '.pgp.private_key'), config('clients.' . $request->client_slug . '.' . env('APP_ENV') . '.pgp.passphrase'))
+            )
         );
         $signature = str_replace('%25', '%', $signature);
         $input_url_encoded_string .= '&signature=' . $signature;
@@ -83,75 +85,16 @@ class AuthorizeCreationController extends Controller
 
     public function sign($query_param, $client_slug)
     {
-        try {
-            // Create temporary isolated keyring directory
-            $tempHome = sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'gpg-' . bin2hex(random_bytes(6));
-            if (!mkdir($tempHome, 0700, true) && !is_dir($tempHome)) {
-                throw new \RuntimeException('Failed to create temp GNUPGHOME');
-            }
-            
-            // Ensure the directory is writable
-            if (!is_writable($tempHome)) {
-                throw new \RuntimeException('Temp GNUPGHOME directory is not writable');
-            }
+        $gpg = new Crypt_GPG([
+            'homedir' => sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'gpg-' . bin2hex(random_bytes(6)),
+            'armor' => true,
+        ]);
+        $egiroService = new EgiroService();
+        $gpg->addSignKey(config('clients.' . $client_slug . '.' . env('APP_ENV') . '.pgp.fingerprint'), config('clients.' . $client_slug . '.' . env('APP_ENV') . '.pgp.passphrase'));
+        $signature = $egiroService->encodeURIComponent(
+            $gpg->sign($query_param, config('clients.' . $client_slug . '.' . env('APP_ENV') . '.pgp.private_key'), config('clients.' . $client_slug . '.' . env('APP_ENV') . '.pgp.passphrase'))
+        );
 
-            try {
-                // Create Crypt_GPG instance with temporary keyring
-                $gpg = new Crypt_GPG([
-                    'homedir' => $tempHome,
-                    'armor' => true  // Enable ASCII armor output
-                ]);
-                
-                // Get configuration
-                $privateKeyPath = config('clients.' . $client_slug . '.' . env('APP_ENV') . '.pgp.private_key');
-                $passphrase = config('clients.' . $client_slug . '.' . env('APP_ENV') . '.pgp.passphrase');
-                $keyFingerprint = config('clients.' . $client_slug . '.' . env('APP_ENV') . '.pgp.fingerprint');
-                
-                // Import the private key
-                if (!is_readable($privateKeyPath)) {
-                    throw new \RuntimeException('Private key not readable at ' . $privateKeyPath);
-                }
-                
-                $armoredKey = file_get_contents($privateKeyPath);
-                
-                // Import the key using the public method
-                $importResult = $gpg->importKey($armoredKey);
-                
-                if (empty($importResult['fingerprint'])) {
-                    throw new \RuntimeException('Failed to import private key');
-                }
-                
-                // Use provided fingerprint or the one from import
-                $keyFingerprint = $keyFingerprint ?: $importResult['fingerprint'];
-                
-                // Add the signing key
-                $gpg->addSignKey($keyFingerprint, $passphrase);
-                
-                // Sign the message with detached signature
-                $signature = $gpg->sign($query_param, Crypt_GPG::SIGN_MODE_DETACHED);
-                
-                if (!$signature || stripos($signature, 'BEGIN PGP SIGNATURE') === false) {
-                    throw new \RuntimeException('Signing returned invalid signature');
-                }
-                
-                // Remove newlines and control characters from signature
-                $signature = preg_replace('/[\r\n\t]/', '', $signature);
-                $signature = preg_replace('/[\x00-\x1F\x7F]/', '', $signature);
-                
-                return $signature;
-                
-            } finally {
-                // Cleanup temporary keyring
-                try {
-                    array_map('unlink', glob($tempHome . DIRECTORY_SEPARATOR . '*') ?: []);
-                    @rmdir($tempHome);
-                } catch (\Throwable $e) {
-                    // Ignore cleanup errors
-                }
-            }
-            
-        } catch (\Exception $e) {
-            throw new \RuntimeException('PGP signing failed: ' . $e->getMessage());
-        }
+        return $signature;
     }
 }
