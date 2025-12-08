5c2746a25ffbf206e478dee0d5a4a6128f911c39
pratik <pratik@sivreninnovations.com>
2025-12-08 14:53:12 +0530

    gffhfhfgh

--- config/clients.php
+++ config/clients.php
@@ -103,6 +103,40 @@ return [
             ]
         ],
     ],
+    'ukcredit' => [
+        'uat' => [
+            'applicant_bank_code' => 'UOVBSGS0XXX',
+            'bo_name' => 'UK CREDIT S PTE LTD',
+            'client_id' => 'BOUKX0474G01UKC',
+            'x-api-key' => 'a177047e-7fd7-4693-9e95-66a528aba3a8',
+            'aggregator_key_alias' => 'AGGREGATOR5_UAT_PGP_PRIMARY',
+            'sign_key_alias' => 'BOUKC201900474G_BU01_UAT_PGP_PUBLICKEY_PRIMARY_377086',
+            'pgp' => [
+                'issuer_id' => 'uobuat@sivren.com',
+                'passphrase' => 'pgpss2025',
+                'fingerprint' => '4CA8AD8EAA3A29E33765A659326E636F574ACFDA',
+                'issuer_key_id' => '326E636F574ACFDA',
+                'public_key' => storage_path('app/pgp/public.asc'),
+                'private_key' => storage_path('app/pgp/private.asc'),
+            ]
+        ],
+        'production' => [
+            'applicant_bank_code' => '',
+            'bo_name' => '',
+            'client_id' => '',
+            'x-api-key' => '',
+            'aggregator_key_alias' => '',
+            'sign_key_alias' => '',
+            'pgp' => [
+                'issuer_id' => '',
+                'passphrase' => '',
+                'fingerprint' => '',
+                'issuer_key_id' => '',
+                'public_key' => storage_path('app/pgp/public.asc'),
+                'private_key' => storage_path('app/pgp/private.asc'),
+            ]
+        ],
+    ],
     'moneywise-credit' => [
         'uat' => [
             'applicant_bank_code' => 'UOVBSGS0XXX',
