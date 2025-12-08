4453c67a630ad06aba32b3ce3908d38d3e19bdc5
pratik <pratik@sivreninnovations.com>
2025-12-08 10:43:03 +0530

    gfhfghfghgfhf

--- app/Http/Controllers/AuthorizeCreationController.php
+++ app/Http/Controllers/AuthorizeCreationController.php
@@ -3,13 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Services\EgiroService;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Str;
 use App\Services\GpgService;
-use Crypt_GPG;
 use Redirect;
 
 class AuthorizeCreationController extends Controller
@@ -27,24 +22,18 @@ class AuthorizeCreationController extends Controller
         $input_url_encoded_string = http_build_query($input_array, null, null, PHP_QUERY_RFC3986);
         $input_url_encoded_string = str_replace('%25', '%', $input_url_encoded_string);
         $input_url_encoded_string = str_replace('%20', ' ', $input_url_encoded_string);
-
         $privateKeyPath = config('clients.' . $request->client_slug . '.' . env('APP_ENV') . '.pgp.private_key');
         $passphrase = config('clients.' . $request->client_slug . '.' . env('APP_ENV') . '.pgp.passphrase');
         $keyFingerprint = config('clients.' . $request->client_slug . '.' . env('APP_ENV') . '.pgp.fingerprint');
-
         $signature = $egiroService->encodeURIComponent(
             $egiroService->encodeURIComponent(
                 $gpgService->sign($input_url_encoded_string, $privateKeyPath, $passphrase, $keyFingerprint)
             )
         );
-        $signature = $egiroService->encodeURIComponent(
-            $gpgService->sign($input_url_encoded_string, $privateKeyPath, $passphrase, $keyFingerprint)
-        );
         $signature = str_replace('%25', '%', $signature);
         $input_url_encoded_string .= '&signature=' . $signature;
         $apiUrl = config('abs.' . env('APP_ENV') . '.authorizeCreation.api_url');
         $fullUrl = $apiUrl . '?' . $input_url_encoded_string;
-
         return Redirect::to($fullUrl);
     }
 }
